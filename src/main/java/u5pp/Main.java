bce02f3f75c4d45eff482a31a710e7ffa9ad2a48
sovaonetrick <iankim8923@gmail.com>
2023-12-06 21:34:43 -0800

    fsfd

--- src/main/java/u5pp/Main.java
+++ src/main/java/u5pp/Main.java
@@ -1,6 +1,128 @@
 package u5pp;
 
 import java.util.Scanner;
+import java.util.Random;
+
+class Dice 
+{
+
+  private int currentSide;
+  private int numSides = 6;
+  private int startingSide = 1;
+  private String color = "green";
+
+public Dice(int numSides, int startingSide, String color)
+{
+  if (startingSide > numSides)
+  {
+    startingSide = this.startingSide;
+  }
+  this.numSides = numSides;
+  this.startingSide = startingSide;
+  this.color = color;
+}
+
+public Dice(int numSides, int startingSide)
+{
+  if (startingSide > numSides)
+  {
+    startingSide = this.startingSide;
+  }
+  this.numSides = numSides;
+  this.startingSide = startingSide;
+
+  String color = this.color;
+}
+
+public Dice(int numSides)
+{
+  if (startingSide > numSides)
+  {
+    startingSide = this.startingSide;
+  }
+  this.numSides = numSides;
+
+  int startingSide = this.startingSide;
+  String color = this.color;
+}
+
+public Dice()
+{
+  if (startingSide > numSides)
+  {
+    startingSide = this.startingSide;
+  }
+  int numSides = this.numSides;
+  int startingSide = this.startingSide;
+  String color = this.color;
+}
+
+public int getSides()
+{
+  return this.numSides;
+}
+
+public int getCurrentSide()
+{
+  return this.startingSide;
+}
+
+public String getColor()
+{
+  return this.color;
+}
+
+public String toString()
+{
+  return "The " + this.color + " " + this.numSides + "-sided dice is showing " + this.startingSide;
+}
+
+public boolean equals(Dice d)
+{
+  currentSide = this.startingSide;
+  if (currentSide == d.startingSide)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+public int roll()
+{
+  Random rand = new Random();
+  int randomnumber = rand.nextInt(1, (this.numSides + 1));
+  this.currentSide = randomnumber;
+  this.startingSide = randomnumber;
+  return this.currentSide;
+}
+
+void changeSide(int num)
+{
+  if (num >= 1 && num <= this.numSides)
+  {
+    this.currentSide = num;
+    this.startingSide = num;
+  }
+}
+
+void changeColor(String color)
+{
+  this.color = color;
+}
+
+void changeNumSides(int num)
+{
+  this.numSides = num;
+}
+}
+
+class Card
+{
+  
+}
 
 class Main {
   public static void main(String[] args) {
